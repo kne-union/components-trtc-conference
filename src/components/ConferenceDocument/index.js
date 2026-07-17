3b0018a4ac41d12d49fefecf7eaade9b8aabf90b
Linzp <sunandmoon001@163.com>
2026-07-17 18:02:07 +0800

    提交一下

--- src/components/ConferenceDocument/index.js
+++ src/components/ConferenceDocument/index.js
@@ -1,6 +1,7 @@
 import RemoteLoader, { createWithRemoteLoader } from '@kne/remote-loader';
+import iFrameResize from '@kne/iframe-resizer';
 import { Flex, Select } from 'antd';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import style from './style.module.scss';
 
 const Files = createWithRemoteLoader({
@@ -30,12 +31,42 @@ const Files = createWithRemoteLoader({
   );
 });
 
+const IframeDocument = ({ url }) => {
+  const ref = useRef(null);
+  useEffect(() => {
+    if (!ref.current || !url) {
+      return;
+    }
+    let resizers;
+    try {
+      // 被嵌入页面需引入 @kne/iframe-resizer 的 contentWindow.js，跨域时高度才能随内容自适应
+      resizers = iFrameResize({ checkOrigin: false }, ref.current);
+    } catch (error) {
+      resizers = null;
+    }
+    return () => {
+      (Array.isArray(resizers) ? resizers : []).forEach(iframe => {
+        try {
+          iframe?.iFrameResizer?.removeListeners?.();
+        } catch (error) {
+          // ignore cleanup errors
+        }
+      });
+    };
+  }, [url]);
+  return (
+    <div className={style['main-iframe-scroller']}>
+      <iframe ref={ref} src={url} title="conference-document" className={style['main-iframe']} allow="clipboard-write" />
+    </div>
+  );
+};
+
 const ConferenceDocument = ({ type, ...props }) => {
   if (type === 'files') {
     return <Files {...props} />;
   }
   if (type === 'iframe') {
-    return <iframe src={props.url} title="conference-document" className={style['main-iframe']} allow="clipboard-write" />;
+    return <IframeDocument url={props.url} />;
   }
   if (type === 'remote-module') {
     const { module, moduleProps, getSpeechInput, onSpeechStart, onSpeechEnd, getEndConferenceCallback } = props;
@@ -51,7 +82,7 @@ const ConferenceDocument = ({ type, ...props }) => {
       />
     );
   }
-  return <div>Conference Document</div>;
+  return null;
 };
 
 export default ConferenceDocument;
